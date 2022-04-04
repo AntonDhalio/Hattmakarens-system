49908e2b8cc7e849342c771a458e032fcb03c136
madelenn <91068216+madelenn@users.noreply.github.com>
2022-04-04 17:54:10 +0200

    Funktion för att lägga till kund existerar

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Hattmakarens_system.Models;
+using Hattmakarens_system.Repositories;
+using Hattmakarens_system.ViewModels;
+
+namespace Hattmakarens_system.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult AddCustomer()
+        {
+            return View();
+        }
+        [HttpPost]
+         public ActionResult AddCustomer(CostumerViewModel customerViewModel)
+        {
+
+            var cusRepo = new CustomerRepository();
+            var customer = new CustomerModels {
+                Adress = customerViewModel.Adress,
+                Name = customerViewModel.Name,
+                Email = customerViewModel.Email,
+                Comment = customerViewModel.Comment,
+                Phone = customerViewModel.Phone
+            };
+            cusRepo.SaveCostumer(customer);
+            return View();
+        }
+    }
+}
