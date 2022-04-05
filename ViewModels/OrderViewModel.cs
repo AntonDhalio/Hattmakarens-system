56b9ded119d51e3b0030fe8aebeb2eb126232e23
Moa Hedlund <83755862+moahedlund@users.noreply.github.com>
2022-04-05 08:45:31 +0200

    Påbörjad createView

--- ViewModels/OrderViewModel.cs
+++ ViewModels/OrderViewModel.cs
@@ -11,20 +11,27 @@ namespace Hattmakarens_system.ViewModels
     public class OrderViewModel
     {
         public int Id { get; set; }
+        [DisplayName("Bild")]
+        public string Path { get; set; }
+        [DisplayName("Datum")]
         public DateTime Date { get; set; }
+        [DisplayName("Expresstillverkning")]
         public bool Priority { get; set; }
-        [DisplayName("Status")]
-        public string Status { get; set; }
-        [DisplayName("Moms")]
-        public double Moms { get; set; }
-        [DisplayName("Totalsumma")]
-        public double TotalSum { get; set; }
+        //[DisplayName("Status")]
+        //public string Status { get; set; }
+        //[DisplayName("Moms")]
+        //public double Moms { get; set; }
+        //[DisplayName("Totalsumma")]
+        //public double TotalSum { get; set; }
+        //[DisplayName("Kommentar")]
+        [DisplayName("Storlek")]
+        public double Size { get; set; }    
         [DisplayName("Kommentar")]
         public string Comment { get; set; }
-        public string UserId { get; set; }
-        public virtual UserModels User { get; set; }
-        public int CustomerId { get; set; }
-        public virtual CustomerModels Customer { get; set; }
-        public virtual ICollection<Hats> Hats { get; set; }
+        //public string UserId { get; set; }
+        //public virtual UserModels User { get; set; }
+        //public int CustomerId { get; set; }
+        //public virtual CustomerModels Customer { get; set; }
+        //public virtual ICollection<Hats> Hats { get; set; }
     }
 }
