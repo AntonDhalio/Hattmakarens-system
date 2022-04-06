04bd23080b3ff5f7a0e5b0324c77b5f41f40f80e
AntonDhalio <83759822+AntonDhalio@users.noreply.github.com>
2022-04-06 15:55:54 +0200

    Fixat sökview, partialview, controller och en model (OBS påbörjat)

---
+++ ViewModels/SearchViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Hattmakarens_system.ViewModels
+{
+    public class SearchViewModel
+    {
+        public int CustomerId { get; set; }
+        [DisplayName("Namn")]
+        public string CustomerName { get; set; }
+        public int OrderId { get; set; }
+        public string OrderStatus { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string MaterialName { get; set; }
+        public string HatModelName { get; set; }
+
+    }
+}
