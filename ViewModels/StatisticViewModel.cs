80a1aaeba05a3507b0890876f3bf383270677526
madelenn <91068216+madelenn@users.noreply.github.com>
2022-04-07 16:01:09 +0200

    en save

---
+++ ViewModels/StatisticViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Hattmakarens_system.Models;
+
+namespace Hattmakarens_system.ViewModels
+{
+    public class StatisticViewModel
+    {
+        public List<OrderModels> orders { get; set; }
+
+    }
+}
