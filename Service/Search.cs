333b0b330113614e42cbe6a2492c2d748818fb99
Sofia Strömberg <83826297+SofiaStro@users.noreply.github.com>
2022-04-08 16:16:06 +0200

    fungerande model-sökning + påbörjad material-sökning

--- Service/Search.cs
+++ Service/Search.cs
@@ -24,33 +24,46 @@ namespace Hattmakarens_system.Service
             }
             else if (searchOption.Equals("material"))
             {
-                //hämta alla ordrar på angivet material
-                //addera alla ordrar till söklistan
+                //BEHÖVER: hämta id från sambandstabell material-hat
                 var materialList = new MaterialRepository().GetAllMaterials();
                 var searchMaterialList = new List<MaterialModels>();
-                var hatlist = new List<Hats>();
+
                 foreach(var material in materialList)
                 {
-                    if(material.Name.Contains(searchString))
+                    if(material.Type.Contains(searchString))
                     {
                         searchMaterialList.Add(material);
                     }
                 }
                 foreach(var material in searchMaterialList)
                 {
-                    foreach(var hat in material.Hats)
+                    foreach(var hat in material.Hats.ToList())
                     {
                         searchOrderList.Add(orderRepository.GetOrder(hat.OrderId));
                     }
                 }
-                
             }
             else if (searchOption.Equals("model"))
             {
-                //hämta alla ordrar på angivet modell
-                //addera alla ordrar till söklistan
+                //FUNGERAR MEN KAN FINSLIPAS
 
+                var modelList = new HatmodelRepository().GetAllHatmodels();
+                var searchModelList = new List<HatModels>();
 
+                foreach (var model in modelList)
+                {
+                    if (model.Name.Contains(searchString))
+                    {
+                        searchModelList.Add(model);
+                    }
+                }
+                foreach (var model in searchModelList)
+                {
+                    foreach (var hat in model.Hats.ToList())
+                    {
+                        searchOrderList.Add(orderRepository.GetOrder(hat.OrderId));
+                    }
+                }
             }
             else if (searchOption.Equals("customer"))
             {
