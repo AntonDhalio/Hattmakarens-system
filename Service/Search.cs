8065a57c631b7d5e2c08ddcda59b471878e30980
AntonDhalio <83759822+AntonDhalio@users.noreply.github.com>
2022-04-11 08:03:36 +0200

    Gjort klart sökfunktion

--- Service/Search.cs
+++ Service/Search.cs
@@ -15,75 +15,108 @@ namespace Hattmakarens_system.Service
 
         public List<OrderModels> GetSearchList(string searchString, string searchOption, string statusOption)
         {
-            var orderList = orderRepository.GetAllOrders();
-            var searchOrderList = new List<OrderModels>();
-
-            if (searchOption is null)
+            using (var db = new ApplicationDbContext())
             {
-                searchOrderList = orderList;
-            }
-            else if (searchOption.Equals("material"))
-            {
-                //BEHÖVER: hämta id från sambandstabell material-hat
-                var materialList = new MaterialRepository().GetAllMaterials();
-                var searchMaterialList = new List<MaterialModels>();
+                var orderList = orderRepository.GetAllOrders();
+                var hatList = new List<Hats>();
+                var searchOrderList = new List<OrderModels>();
+                var finalList = new List<OrderModels>();
 
-                foreach(var material in materialList)
+
+                if (searchOption is null)
                 {
-                    if(material.Type.Contains(searchString))
+                    searchOrderList = orderList;
+                }
+                else if (searchOption.Equals("material"))
+                {
+                    //BEHÖVER: hämta id från sambandstabell material-hat
+                    var materialList = new MaterialRepository().GetAllMaterials();
+                    var searchMaterialList = new List<MaterialModels>();
+
+                    foreach (var material in materialList)
                     {
-                        searchMaterialList.Add(material);
+                        if (material.Type.Contains(searchString))
+                        {
+                            searchMaterialList.Add(material);
+                        }
+                    }
+
+                    foreach (var material in searchMaterialList)
+                    {
+
+                        var hattar = db.Material.Single(c => c.Id == material.Id).Hats.ToList();
+
+                        foreach (var hat in hatList)
+                        {
+                            searchOrderList.Add(orderRepository.GetOrder(hat.OrderId));
+                        }
+
                     }
                 }
-                foreach(var material in searchMaterialList)
+                else if (searchOption.Equals("model"))
                 {
-                    foreach(var hat in material.Hats.ToList())
+                    //FUNGERAR MEN KAN FINSLIPAS
+
+                    var modelList = new HatmodelRepository().GetAllHatmodels();
+                    var searchModelList = new List<HatModels>();
+
+                    foreach (var model in modelList)
                     {
-                        searchOrderList.Add(orderRepository.GetOrder(hat.OrderId));
+                        if (model.Name.Contains(searchString))
+                        {
+                            searchModelList.Add(model);
+                        }
+                    }
+                    foreach (var model in searchModelList)
+                    {
+                        foreach (var hat in model.Hats.ToList())
+                        {
+                            searchOrderList.Add(orderRepository.GetOrder(hat.OrderId));
+                        }
                     }
                 }
-            }
-            else if (searchOption.Equals("model"))
-            {
-                //FUNGERAR MEN KAN FINSLIPAS
-
-                var modelList = new HatmodelRepository().GetAllHatmodels();
-                var searchModelList = new List<HatModels>();
-
-                foreach (var model in modelList)
+                else if (searchOption.Equals("customer"))
                 {
-                    if (model.Name.Contains(searchString))
+                    //hämta alla ordrar på angivet kundnamn
+                    //addera alla ordrar till söklistan
+                    var CustomerList = new CustomerRepository().GetAllCostumers();
+                    var searchCustomerList = new List<CustomerModels>();
+
+                    foreach (var customer in CustomerList)
                     {
-                        searchModelList.Add(model);
+                        if (customer.Name.Contains(searchString))
+                        {
+                            searchCustomerList.Add(customer);
+                        }
+                    }
+
+                    foreach (var customer in searchCustomerList)                     
+                    {
+                        foreach (var order in orderList)
+                        {
+                            if(customer.Id == order.CustomerId)
+                            {
+                                searchOrderList.Add(orderRepository.GetOrder(order.Id));
+                            }
+                        }
                     }
                 }
-                foreach (var model in searchModelList)
+                if (statusOption.Equals("active"))
                 {
-                    foreach (var hat in model.Hats.ToList())
-                    {
-                        searchOrderList.Add(orderRepository.GetOrder(hat.OrderId));
-                    }
+                    //söklistan ska filtreras på aktiva ordrar
+                    finalList = searchOrderList.Where(c => c.Status == "active").ToList();
                 }
-            }
-            else if (searchOption.Equals("customer"))
-            {
-                //hämta alla ordrar på angivet kundnamn
-                //addera alla ordrar till söklistan
+                if (statusOption.Equals("inactive"))
+                {
+                    //Söklistan ska filtreras på inaktiva ordrar
+                    finalList = searchOrderList.Where(c => c.Status == "inactive").ToList();
+
+                }
+
+                var searchList = finalList.Distinct().ToList();
+                return searchList;
             }
 
-            if (statusOption.Equals("active"))
-            {
-                //söklistan ska filtreras på aktiva ordrar
-            }
-            if (statusOption.Equals("inactive"))
-            {
-                //Söklistan ska filtreras på inaktiva ordrar
-            }
-            
-            var searchList = searchOrderList.Distinct().ToList();
-            return searchList;
         }
-       
-
     }
 }
