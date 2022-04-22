7a8e63c175d80a4ced5dbec38747717c427ddfe0
Moa Hedlund <83755862+moahedlund@users.noreply.github.com>
2022-04-22 11:25:38 +0200

    unikt namn på hattmodell

--- Controllers/HatmodelController.cs
+++ Controllers/HatmodelController.cs
@@ -46,45 +46,53 @@ namespace Hattmakarens_system.Controllers
 
             if (valdMaterial.Count != 0)
             {
-                if (ModelState.IsValid)
+                if (hatModelRepository.ExistingHatModelName(hatmodel.Name) == false)
                 {
-                    var newHatmodel = new HatModels
+                    if (ModelState.IsValid)
                     {
-                        Name = hatmodel.Name,
-                        Description = hatmodel.Description,
-                        Price = hatmodel.Price,
-                        Material = new List<MaterialModels>(),
-                        Images = new List<ImageModels>()
-                    };
-                    if (file != null)
-                    {
-                        string filename = Path.GetFileName(file.FileName);
-                        string imagePath = Path.Combine(Server.MapPath("~/Images"), filename);
-                        var image = new ImageModels
+                        var newHatmodel = new HatModels
                         {
-                            Path = imagePath,
-                            HatModels = new List<HatModels>()
+                            Name = hatmodel.Name,
+                            Description = hatmodel.Description,
+                            Price = hatmodel.Price,
+                            Material = new List<MaterialModels>(),
+                            Images = new List<ImageModels>()
                         };
-                        var imgRepo = new ImageRepository();
-                        newHatmodel.Images.Add(imgRepo.SaveImage(image));
-                    }
-                    using (var context = new ApplicationDbContext())
-                    {
-                        foreach (var Id in valdMaterial)
+                        if (file != null)
                         {
-                            newHatmodel.Material.Add(MaterialRepository.GetMaterial(Id));
+                            string filename = Path.GetFileName(file.FileName);
+                            string imagePath = Path.Combine(Server.MapPath("~/Images"), filename);
+                            var image = new ImageModels
+                            {
+                                Path = imagePath,
+                                HatModels = new List<HatModels>()
+                            };
+                            var imgRepo = new ImageRepository();
+                            newHatmodel.Images.Add(imgRepo.SaveImage(image));
                         }
-                        context.HatModels.Add(newHatmodel);
-                        context.SaveChanges();
-                        TygMaterial = new Service.Material().ResetTygList(TygMaterial);
-                        DekorationMaterial = new Service.Material().ResetDecorationList(DekorationMaterial);
-                        TrådMaterial = new Service.Material().ResetTradList(TrådMaterial);
+                        using (var context = new ApplicationDbContext())
+                        {
+                            foreach (var Id in valdMaterial)
+                            {
+                                newHatmodel.Material.Add(MaterialRepository.GetMaterial(Id));
+                            }
+                            context.HatModels.Add(newHatmodel);
+                            context.SaveChanges();
+                            TygMaterial = new Service.Material().ResetTygList(TygMaterial);
+                            DekorationMaterial = new Service.Material().ResetDecorationList(DekorationMaterial);
+                            TrådMaterial = new Service.Material().ResetTradList(TrådMaterial);
+                        }
+                        return RedirectToAction("Hatmodel", "Hatmodel", new { IsAdded = true });
                     }
-                    return RedirectToAction("Hatmodel", "Hatmodel", new { IsAdded = true });
+                    else
+                    {
+                        ViewBag.MaterialsToPickFrom = new Service.Material().GetSelectListMaterials();
+                        return View(hatmodel);
+                    }
                 }
                 else
                 {
-                    ViewBag.MaterialsToPickFrom = new Service.Material().GetSelectListMaterials();
+                    ViewBag.Message = "Det finns redan en hatmodell med detta namn";
                     return View(hatmodel);
                 }
             }
